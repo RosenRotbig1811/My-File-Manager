7b2e7ab49da8265676798f3ce333b9f8107b241f
RosenRotbig1811 <RosenRotbig1811@users.noreply.github.com>
2024-02-22 01:37:12 +0300

    Initial commit

---
+++ My File Manager/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace My_File_Manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.New_Folder = new System.Windows.Forms.Button();
+            this.label_Progress = new System.Windows.Forms.Label();
+            this.RefreshBtn = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.splitter2 = new System.Windows.Forms.Splitter();
+            this.Copy = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.Delete = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.panel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.BackColor = System.Drawing.SystemColors.Window;
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.treeView1.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.imageList1;
+            this.treeView1.LabelEdit = true;
+            this.treeView1.Location = new System.Drawing.Point(0, 24);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(295, 565);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeExpand);
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_OnAfterSelect);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Internal.png");
+            this.imageList1.Images.SetKeyName(1, "System Folder.png");
+            this.imageList1.Images.SetKeyName(2, "Ocean Blue Open.png");
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.New_Folder);
+            this.panel1.Controls.Add(this.label_Progress);
+            this.panel1.Controls.Add(this.RefreshBtn);
+            this.panel1.Controls.Add(this.progressBar1);
+            this.panel1.Controls.Add(this.splitter2);
+            this.panel1.Controls.Add(this.Copy);
+            this.panel1.Controls.Add(this.listView1);
+            this.panel1.Controls.Add(this.Delete);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.panel1.Location = new System.Drawing.Point(295, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(529, 565);
+            this.panel1.TabIndex = 2;
+            // 
+            // New_Folder
+            // 
+            this.New_Folder.Location = new System.Drawing.Point(122, 468);
+            this.New_Folder.Name = "New_Folder";
+            this.New_Folder.Size = new System.Drawing.Size(101, 23);
+            this.New_Folder.TabIndex = 12;
+            this.New_Folder.Text = "New Folder";
+            this.New_Folder.UseVisualStyleBackColor = true;
+            this.New_Folder.Click += new System.EventHandler(this.New_Folder_Click);
+            // 
+            // label_Progress
+            // 
+            this.label_Progress.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_Progress.AutoSize = true;
+            this.label_Progress.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_Progress.Location = new System.Drawing.Point(356, 502);
+            this.label_Progress.Name = "label_Progress";
+            this.label_Progress.Size = new System.Drawing.Size(65, 19);
+            this.label_Progress.TabIndex = 11;
+            this.label_Progress.Text = "Progress";
+            // 
+            // RefreshBtn
+            // 
+            this.RefreshBtn.Location = new System.Drawing.Point(4, 529);
+            this.RefreshBtn.Name = "RefreshBtn";
+            this.RefreshBtn.Size = new System.Drawing.Size(75, 23);
+            this.RefreshBtn.TabIndex = 10;
+            this.RefreshBtn.Text = "RefreshBtn";
+            this.RefreshBtn.UseVisualStyleBackColor = true;
+            this.RefreshBtn.Click += new System.EventHandler(this.Refresh_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(249, 533);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(273, 25);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // splitter2
+            // 
+            this.splitter2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.splitter2.Location = new System.Drawing.Point(0, 453);
+            this.splitter2.Name = "splitter2";
+            this.splitter2.Size = new System.Drawing.Size(525, 10);
+            this.splitter2.TabIndex = 8;
+            this.splitter2.TabStop = false;
+            // 
+            // Copy
+            // 
+            this.Copy.Location = new System.Drawing.Point(4, 469);
+            this.Copy.Name = "Copy";
+            this.Copy.Size = new System.Drawing.Size(75, 23);
+            this.Copy.TabIndex = 6;
+            this.Copy.Text = "Copy to";
+            this.Copy.UseVisualStyleBackColor = true;
+            this.Copy.Click += new System.EventHandler(this.Copy_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.LargeImageList = this.imageList2;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(525, 453);
+            this.listView1.SmallImageList = this.imageList2;
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_changed);
+            this.listView1.Click += new System.EventHandler(this.listView1_Click);
+            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Имя";
+            this.columnHeader1.Width = 217;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Размер";
+            this.columnHeader2.Width = 124;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Изменен";
+            this.columnHeader3.Width = 87;
+            // 
+            // imageList2
+            // 
+            this.imageList2.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList2.ImageStream")));
+            this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList2.Images.SetKeyName(0, "System Folder.png");
+            this.imageList2.Images.SetKeyName(1, "Generic Document.png");
+            this.imageList2.Images.SetKeyName(2, "Meta Package.png");
+            this.imageList2.Images.SetKeyName(3, "help-46.png");
+            this.imageList2.Images.SetKeyName(4, "Text Edit.png");
+            this.imageList2.Images.SetKeyName(5, "Bean.png");
+            this.imageList2.Images.SetKeyName(6, "w256h2561337442069fichiersmp3.png");
+            this.imageList2.Images.SetKeyName(7, "Image Capture.png");
+            this.imageList2.Images.SetKeyName(8, "w256h2561350597263avi.png");
+            this.imageList2.Images.SetKeyName(9, "images.jpg");
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(4, 500);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(75, 23);
+            this.Delete.TabIndex = 5;
+            this.Delete.Text = "Delete File";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(824, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(295, 24);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 565);
+            this.splitter1.TabIndex = 5;
+            this.splitter1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 589);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "My File Explorer";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ImageList imageList2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.Button Copy;
+        private System.Windows.Forms.Splitter splitter2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.Button RefreshBtn;
+        private System.Windows.Forms.Label label_Progress;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button New_Folder;
+    }
+}
+
---
+++ My File Manager/Form1.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace My_File_Manager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DriveTreeInit();
+        }
+
+        public void DriveTreeInit()
+        {
+            string[] drivesArray = Directory.GetLogicalDrives();
+
+            treeView1.BeginUpdate();
+            treeView1.Nodes.Clear();
+
+            foreach (string s in drivesArray)
+            {
+                TreeNode drive = new TreeNode(s, 0, 0);
+                treeView1.Nodes.Add(drive);
+
+                GetDirs(drive);
+            }
+
+
+            treeView1.EndUpdate();
+        }
+
+        public void GetDirs(TreeNode node)
+        {
+            DirectoryInfo[] diArray;
+
+            node.Nodes.Clear();
+
+            string fullPath = node.FullPath;
+            DirectoryInfo di = new DirectoryInfo(fullPath);
+
+            try
+            {
+                diArray = di.GetDirectories();
+            }
+            catch
+            {
+                return;
+            }
+
+            foreach (DirectoryInfo dirinfo in diArray)
+            {
+                TreeNode dir = new TreeNode(dirinfo.Name, 1, 2);
+                node.Nodes.Add(dir);
+            }
+        }
+
+        private void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            treeView1.BeginUpdate();
+
+            foreach (TreeNode node in e.Node.Nodes)
+            {
+                GetDirs(node);
+            }
+
+            treeView1.EndUpdate();
+        }
+
+        public void treeView1_OnAfterSelect(object sender, System.Windows.Forms.TreeViewEventArgs e)
+        {
+            TreeNode selectedNode = e.Node;
+            string fullPath = selectedNode.FullPath;
+            DirectoryInfo di = new DirectoryInfo(fullPath);
+            FileInfo[] fiArray;
+            DirectoryInfo[] diArray;
+
+            try
+            {
+                fiArray = di.GetFiles();
+                diArray = di.GetDirectories();
+            }
+            catch
+            {
+                return;
+            }
+
+            listView1.Items.Clear();
+
+            foreach (DirectoryInfo dirInfo in diArray)
+            {
+                ListViewItem lvi = new ListViewItem(dirInfo.Name);
+                lvi.SubItems.Add("0");
+                lvi.SubItems.Add(dirInfo.LastWriteTime.ToString());
+                lvi.ImageIndex = 0;
+                listView1.Items.Add(lvi);
+            }
+
+            foreach (FileInfo fileInfo in fiArray)
+            {
+                ListViewItem lvi = new ListViewItem(fileInfo.Name);
+                lvi.SubItems.Add(fileInfo.Length.ToString() + " Кб");
+                lvi.SubItems.Add(fileInfo.LastWriteTime.ToString());
+
+                string filenameExtension = Path.GetExtension(fileInfo.Name).ToLower();
+
+                switch (filenameExtension)
+                {
+                    case ".com":
+                        {
+                            lvi.ImageIndex = 2;
+                            break;
+                        }
+                    case ".exe":
+                        {
+                            lvi.ImageIndex = 2;
+                            break;
+                        }
+                    case ".hlp":
+                        {
+                            lvi.ImageIndex = 3;
+                            break;
+                        }
+                    case ".txt":
+                        {
+                            lvi.ImageIndex = 4;
+                            break;
+                        }
+                    case ".doc":
+                        {
+                            lvi.ImageIndex = 5;
+                            break;
+                        }
+                    case ".mp3":
+                        {
+                            lvi.ImageIndex = 6;
+                            break;
+                        }
+                    case ".jpg":
+                        {
+                            lvi.ImageIndex = 7;
+                            break;
+                        }
+                    case ".avi":
+                        {
+                            lvi.ImageIndex = 8;
+                            break;
+                        }
+                    case ".zip":
+                        {
+                            lvi.ImageIndex = 9;
+                            break;
+                        }
+                    case ".rar":
+                        {
+                            lvi.ImageIndex = 9;
+                            break;
+                        }
+                    case ".docx":
+                        {
+                            lvi.ImageIndex = 5;
+                            break;
+                        }
+                    default:
+                        {
+                            lvi.ImageIndex = 1;
+                            break;
+                        }
+                }
+
+                listView1.Items.Add(lvi);
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            //string filename = listView1.SelectedItems[0].Tag.ToString();
+
+            
+            Process.Start(Path.Combine(treeView1.SelectedNode.FullPath.ToString(), listView1.Items[listView1.SelectedIndices[0]].Text));
+
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string message = "My File Manager ONPU 2013\n\nVasiliy Kuzmenko";
+            MessageBox.Show(message);
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+
+            listView1.BeginUpdate();
+            string File_to_Delete = "";
+            string Folder_to_Delete = "";
+            string message = "Вы действительно хотите удалить файл?";
+            const string caption = "Файл будет удален";
+            var rezult = MessageBox.Show(message, caption, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (rezult == DialogResult.Yes)
+            {
+                if (listView1.Items[listView1.SelectedIndices[0]].Text != "")
+                {
+                    //Delete.Enabled = true;
+                    File_to_Delete = Path.Combine(treeView1.SelectedNode.FullPath.ToString(), listView1.Items[listView1.SelectedIndices[0]].Text);
+                }
+
+
+
+                Folder_to_Delete = Path.Combine(treeView1.SelectedNode.FullPath.ToString(), listView1.Items[listView1.SelectedIndices[0]].Text);
+
+
+                System.IO.DirectoryInfo directory = new System.IO.DirectoryInfo(Folder_to_Delete);
+                System.IO.FileInfo file = new System.IO.FileInfo(File_to_Delete);
+
+                try
+                {
+                    if (File.Exists(File_to_Delete))
+                    {
+                        file.Delete();
+                    }
+                    else
+                    {                        
+                        directory.Delete(true);
+                    }
+                }
+                catch (System.IO.IOException) { }
+
+                
+
+
+                for (int i = 0; i <= 10000; i++)
+                {
+                    progressBar1.Value = i * 100 / 10000;
+                }
+ 
+            }
+
+
+            
+            listView1.Items.RemoveAt(listView1.SelectedIndices[0]);
+            treeView1.Nodes.Remove(treeView1.SelectedNode);
+            DriveTreeInit();
+            listView1.EndUpdate();
+        }
+
+        private void Copy_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+
+
+            string Path_Before = (Path.Combine(treeView1.SelectedNode.FullPath.ToString(), listView1.Items[listView1.SelectedIndices[0]].Text));
+            string Name = listView1.Items[listView1.SelectedIndices[0]].Text;
+            string Path_To_Copy = Path.Combine(folderBrowserDialog1.SelectedPath, Name);
+
+            if (!File.Exists(Path.Combine(folderBrowserDialog1.SelectedPath, Name)))
+            {
+                File.Copy(Path_Before, Path_To_Copy);
+            }
+            else
+            {
+                string message = "Такой файл уже существует";
+                //const string caption = "Файл будет удален";
+                var rezult = MessageBox.Show(message);
+
+            }
+
+            for (int i = 0; i <= 10000; i++)
+            {
+                progressBar1.Value = i * 100 / 10000;
+            }
+        }
+
+        private void Refresh_Click(object sender, EventArgs e)
+        {
+            DriveTreeInit();
+        }
+
+        private void listView1_Click(object sender, EventArgs e)
+        {
+
+            /*if (listView1.SelectedItems.Count > 0)
+            {
+                Delete.Enabled = true;
+            }
+            else
+            {
+                Delete.Enabled = false;
+            }*/
+
+        }
+
+        private void New_Folder_Click(object sender, EventArgs e)
+        {
+
+            listView1.BeginUpdate();
+
+            string path = System.IO.Path.Combine(Path.Combine(treeView1.SelectedNode.FullPath.ToString()), "New Folder");
+            System.IO.Directory.CreateDirectory(path);
+            listView1.Items.Add("New Folder", 1);
+            DriveTreeInit();
+
+            listView1.EndUpdate();
+        }
+
+        private void listView1_changed(object sender, EventArgs e)
+        {
+            Delete.Enabled = true;
+        }
+    }
+}
+
+
+ 
+    
+
+
